77192e3290d3bd64468acd7919aaf9795c0cc464
Dmitry O Litvintsev <dmitrylitvintsev@mac-131313.attlocal.net>
2020-04-03 17:14:28 -0500

    dataspace: added Reaper class that provided old data removal functionality and a stand-alone script that uses it

--- framework/dataspace/tests/test_Reaper.py
+++ framework/dataspace/tests/test_Reaper.py
@@ -1,9 +1,7 @@
 import mock
-import os
-import pytest
+import time
 import unittest
 
-import decisionengine.framework.dataspace.datasource as datasource
 import decisionengine.framework.dataspace.dataspace as dataspace
 
 GLOBAL_CONFIG = {
@@ -34,15 +32,59 @@ class MockSource(object):
 class TestReaper(unittest.TestCase):
 
     def setUp(self):
-        self.reaper = dataspace.Reaper(GLOBAL_CONFIG)
+        with mock.patch.object(dataspace.DataSourceLoader, "create_datasource") as source:
+            source.return_value = MockSource()
+            GLOBAL_CONFIG["dataspace"]["retention_interval_in_days"] = 365
+            self.reaper = dataspace.Reaper(GLOBAL_CONFIG)
 
     def tearDown(self):
         pass
 
-    def test_reaper(self):
-        with mock.patch.object(dataspace.DataSourceLoader, "create_datasource") as source:
-            source.return_value = MockSource()
+    def test_reap(self):
+        self.assertEqual(self.reaper.get_state(),
+                         dataspace.State.IDLE)
         self.reaper.reap()
+
+    def test_start_stop(self):
+        self.reaper.start()
+        self.assertTrue(self.reaper.get_state() in (dataspace.State.RUNNING, dataspace.State.SLEEPING))
+        self.reaper.stop()
+        self.assertEqual(self.reaper.get_state(), dataspace.State.STOPPED)
+
+    def test_loop(self):
+        for _ in range(3):
+            print (self.reaper)
+            self.test_start_stop()
+            time.sleep(1)
+
+    def test_fail_missing_config_key(self):
+        with self.assertRaises(dataspace.DataSpaceConfigurationError):
+            with mock.patch.object(dataspace.DataSourceLoader, "create_datasource") as source:
+                source.return_value = MockSource()
+                del GLOBAL_CONFIG["dataspace"]["retention_interval_in_days"]
+                dataspace.Reaper(GLOBAL_CONFIG)
+
+    def test_fail_wrong_config_key(self):
+        with self.assertRaises(ValueError):
+            with mock.patch.object(dataspace.DataSourceLoader, "create_datasource") as source:
+                source.return_value = MockSource()
+                GLOBAL_CONFIG["dataspace"]["retention_interval_in_days"] = "abc"
+                dataspace.Reaper(GLOBAL_CONFIG)
+
+    def test_source_fail(self):
+        with mock.patch.object(MockSource, "delete_old_data") as function:
+            function.side_effect = KeyError
+            self.reaper.start()
+            self.assertEqual(self.reaper.get_state(), dataspace.State.ERROR)
+            self.reaper.stop()
+            self.assertEqual(self.reaper.get_state(), dataspace.State.ERROR)
+            function.side_effect = None
+            self.reaper.start()
+            print (self.reaper.get_state())
+            self.assertTrue(self.reaper.get_state() in (dataspace.State.RUNNING, dataspace.State.SLEEPING))
+            self.reaper.stop()
+            self.assertEqual(self.reaper.get_state(), dataspace.State.STOPPED)
+
 
 if __name__ == '__main__':
     unittest.main()
