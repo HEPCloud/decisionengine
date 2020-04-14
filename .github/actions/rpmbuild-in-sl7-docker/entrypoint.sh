1c8f306346464bf51eb1446d9d4754a1b6a0362b
Patrick Gartung <gartung@fnal.gov>
2020-04-14 18:30:09 -0500

    Test install of rpm.

--- .github/actions/rpmbuild-in-sl7-docker/entrypoint.sh
+++ .github/actions/rpmbuild-in-sl7-docker/entrypoint.sh
@@ -5,4 +5,5 @@ echo PYVER=$PYVER
 decisionengine/build/packaging/rpm/package.sh decisionengine
 status=$?
 tar cf $GITHUB_WORKSPACE/rpmbuild-$PYVER.tar /var/tmp/`whoami`/rpm/decisionengine/*RPMS
+rpm -iv /var/tmp/`whoami`/rpm/decisionengine/RPMS/*/*.rpm
 exit $status
