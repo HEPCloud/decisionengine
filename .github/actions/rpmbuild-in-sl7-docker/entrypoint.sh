84131bd3df14ca9a2463c81309597c52a0bf144d
Patrick Gartung <gartung@fnal.gov>
2020-04-14 19:07:22 -0500

    Use correct spec file for PYVER=3.6

--- .github/actions/rpmbuild-in-sl7-docker/entrypoint.sh
+++ .github/actions/rpmbuild-in-sl7-docker/entrypoint.sh
@@ -2,7 +2,7 @@
 PYVER=${1:-"2.7"}
 export PYVER
 echo PYVER=$PYVER
-decisionengine/build/packaging/rpm/package.sh decisionengine
+env PYVER=$PYVER decisionengine/build/packaging/rpm/package.sh decisionengine
 status=$?
 tar cf $GITHUB_WORKSPACE/rpmbuild-$PYVER.tar /var/tmp/`whoami`/rpm/decisionengine/*RPMS
 rpm -iv /var/tmp/`whoami`/rpm/decisionengine/RPMS/*/*.rpm
