5578920386bcb8c4b456eb3591bcc824cd07b423
Dmitry O Litvintsev <dmitrylitvintsev@mac-131313.dhcp.fnal.gov>
2020-04-29 17:21:18 -0500

    use assertEqual for uniformity

--- framework/dataspace/tests/test_datablock.py
+++ framework/dataspace/tests/test_datablock.py
@@ -20,14 +20,14 @@ class TestDatablock(unittest.TestCase):
                                                             protocol=pickle.HIGHEST_PROTOCOL)})
         value = ast.literal_eval(datablock.decompress(zstring))
         value = datablock.zloads(value.get('value'))
-        assert value, self.obj
+        self.assertEqual(value, self.obj)
 
         zstring = datablock.compress({'pickled': True,
                                       'value': zlib.compress(pickle.dumps(self.obj,
                                                                           protocol=pickle.HIGHEST_PROTOCOL), 9)})
         value = ast.literal_eval(datablock.decompress(zstring))
         value = datablock.zloads(value.get('value'))
-        assert value, self.obj
+        self.assertEqual(value, self.obj)
 
 
 if __name__ == "__main__":
