48f58b64e9bcf48328238d100eaf846431bf5077
Bruno Coimbra <coimbra@fnal.gov>
2020-04-29 15:04:46 -0500

    Make postgresql tests independent

--- framework/dataspace/datasources/tests/test_postgresql.py
+++ framework/dataspace/datasources/tests/test_postgresql.py
@@ -11,31 +11,46 @@ if "PG_CTL" in os.environ:
 
 postgresql_server = factories.postgresql_proc(pg_ctl)
 
-@pytest.fixture(scope="session")
-def postgresql(postgresql_server):
+@pytest.fixture(scope="function")
+def postgresql(postgresql_server, data):
     dsn = {
         "user": postgresql_server.user,
         "port": postgresql_server.port,
         "host": postgresql_server.unixsocketdir
     }
-    # Load decision engine schema
+
     with psycopg2.connect(**dsn) as connection:
         with connection.cursor() as cursor:
             cwd = os.path.split(os.path.abspath(__file__))[0]
+            # Load decision engine schema
             cursor.execute(open(cwd + "/../postgresql.sql", "r").read())
+            # Load test data
+            for table, rows in data.items():
+                for row in rows:
+                    keys = ",".join(row.keys())
+                    values = ",".join([f"'{value}'" for value in row.values()])
+                    cursor.execute(f"INSERT INTO {table} ({keys}) VALUES ({values})")
         connection.commit()
 
     return Postgresql(dsn)
 
 @pytest.fixture(scope="session")
-def taskmanager():
+def data():
     return {
-        "name": "test",
-        "taskmanager_id": "12321"
+        "taskmanager": [
+            {
+                "name": "taskmanager1",
+                "taskmanager_id": "1"
+            }
+        ]
     }
 
-def test_py_psql(postgresql):
-    print(postgresql)
+@pytest.fixture(scope="session")
+def taskmanager():
+    return {
+        "name": "new_taskmanager",
+        "taskmanager_id": "123"
+    }
 
 def test_generate_insert_query():
     table_name = "header"
@@ -51,8 +66,24 @@ def test_create_tables(postgresql):
 
 def test_store_taskmanager(postgresql, taskmanager):
     result = postgresql.store_taskmanager(taskmanager["name"], taskmanager["taskmanager_id"])
-    assert result == 1
+    assert result > 0
 
-def test_get_taskmanager(postgresql, taskmanager):
-    result = postgresql.get_taskmanager(taskmanager["name"], taskmanager["taskmanager_id"])
-    pass
+def test_get_taskmanager(postgresql, taskmanager, data):
+    # test valid taskmanager
+    result = postgresql.get_taskmanager(data["taskmanager"][0]["name"], data["taskmanager"][0]["taskmanager_id"])
+    assert result["name"] == data["taskmanager"][0]["name"]
+    assert result["taskmanager_id"] == data["taskmanager"][0]["taskmanager_id"]
+
+    result = postgresql.get_taskmanager(data["taskmanager"][0]["name"])
+    assert result["name"] == data["taskmanager"][0]["name"]
+
+    # test taskmanager not present in the database
+    try:
+        result = postgresql.get_taskmanager(taskmanager["name"], taskmanager["taskmanager_id"])
+    except Exception as e:
+        assert e.__class__ == KeyError
+    
+    try:
+        result = postgresql.get_taskmanager(taskmanager["name"])
+    except Exception as e:
+        assert e.__class__ == KeyError
