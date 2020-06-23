e66402b3f64dd0c48e12e1bccb633bae6240701c
Kyle Knoepfel <knoepfel@fnal.gov>
2020-06-23 10:02:23 -0500

    Further config-manager testing and factorizations.

--- framework/configmanager/ConfigManager.py
+++ framework/configmanager/ConfigManager.py
@@ -12,13 +12,15 @@ MANDATORY_MODULE_KEYS = {"module", "name", "parameters"}
 
 CONFIG_FILE_NAME = "decision_engine.conf"
 
-def _config_from_file(config_file):
+def _verify_not_empty(config_file):
     if os.path.getsize(config_file) == 0:
         raise RuntimeError(f"Empty configuration file {config_file}")
 
+def _config_from_file(config_file):
+    _verify_not_empty(config_file)
     global_config = None
     try:
-        with open(config_file, "r") as f:
+        with open(config_file) as f:
             try:
                 global_config = eval(f.read())
             except Exception as msg:
@@ -30,121 +32,127 @@ def _config_from_file(config_file):
         raise RuntimeError("The configuration file must be a Python dictionary.")
     return global_config
 
+def _make_logger(global_config):
+    if 'logger' not in global_config:
+        raise RuntimeError("No logger configuration has been specified.")
+    try:
+        logger_config = global_config['logger']
+        de_logger.set_logging(log_file_name=logger_config['log_file'],
+                              max_file_size=logger_config['max_file_size'],
+                              max_backup_count=logger_config['max_backup_count'],
+                              log_level=logger_config.get('log_level', 'WARNING'))
+        return de_logger.get_logger()
+    except Exception as msg:
+        raise RuntimeError(f"Failed to create log: {msg}")
+
+def _check_keys(channel_conf_dict):
+    """
+    check that channel config has mandatory keys
+    :type data: :obj:`dict`
+    """
+    channel_keys = set(channel_conf_dict.keys())
+    diff = MANDATORY_CHANNEL_KEYS - channel_keys
+    if diff:
+        missing = list(diff)
+        raise RuntimeError(f"channel is missing one or more mandatory keys {missing}")
+    for name in MANDATORY_CHANNEL_KEYS:
+        conf = channel_conf_dict[name]
+        for module_name, module_conf in conf.items():
+            try:
+                module_keys = set(module_conf.keys())
+            except Exception as msg:
+                raise RuntimeError(f"{name} module {module_name} is not a dictionary, {msg}")
+            diff = MANDATORY_MODULE_KEYS - module_keys
+            if diff:
+                missing_keys = str(list(diff))
+                raise RuntimeError(f"{name} module {module_name} is missing one or more mandatory keys {missing_keys} ")
+
+def _validate_channel(channel):
+    """
+    Validate channels
+    :type channel: :obj:`dict`
+    """
+    _check_keys(channel)
+
+    # Sources
+    sources = channel.get('sources')
+    all_produces = set()
+    for sname, conf in sources.items():
+        my_module = importlib.import_module(conf.get('module'))
+        try:
+            produces = getattr(my_module, 'PRODUCES')
+            all_produces |= set(produces)
+        except AttributeError:
+            raise RuntimeError(f"source module {sname} does not have required PRODUCES list")
+
+    all_consumes = set()
+
+    # Transforms
+    transforms = channel.get('transforms')
+    transform_map = {}
+
+    for tname, conf in transforms.items():
+        my_module = importlib.import_module(conf.get('module'))
+        try:
+            consumes = set(getattr(my_module, 'CONSUMES'))
+            produces = set(getattr(my_module, 'PRODUCES'))
+            all_consumes |= consumes
+            all_produces |= produces
+            transform_map[tname] = {'consumes': consumes,
+                                    'produces': produces}
+        except AttributeError as msg:
+            raise RuntimeError(f"transform module {tname} does not have required lists {msg}")
+
+    if not all_consumes.issubset(all_produces):
+        extra_keys = list(all_consumes - all_produces)
+        raise RuntimeError(f"consumes are not subset of produce, extra keys {extra_keys}")
+
+    # graph contains pairs of modules and lists of modules that depends on
+    # this module, e.g.:
+    # { "A" : [B,C,D],
+    # "D" : [C,B] }
+    graph = {}
+    for i in range(len(transform_map)):
+        k1 = list(transform_map.keys())[i]
+        c1 = set(transform_map[k1].get('consumes', []))
+        p1 = set(transform_map[k1].get('produces', []))
+        added = False
+        for j in range(i + 1, len(transform_map)):
+            k2 = list(transform_map.keys())[j]
+            c2 = set(transform_map[k2].get('consumes', []))
+            p2 = set(transform_map[k2].get('produces', []))
+            if c2 & p1:
+                added = True
+                graph.setdefault(k1, []).append(k2)
+            if p2 & c1:
+                added = True
+                graph.setdefault(k2, []).append(k1)
+        if not added:
+            graph.setdefault(k1, [])
+
+    # sort modules using topological sort
+    # sorted_modules are transform modules in order of execution
+    sorted_modules, cyclic_modules = tsort.tsort(graph)
+
+    if cyclic_modules:
+        raise RuntimeError(f"cyclic dependency detected for modules {list(cyclic_modules)}")
+
 
 class ConfigManager():
 
-    def __init__(self, config_file_name=CONFIG_FILE_NAME):
+    def __init__(self):
         self.config_dir = os.getenv("CONFIG_PATH", "/etc/decisionengine")
         if not os.path.isdir(self.config_dir):
-            raise Exception("Config dir '%s' not found" % self.config_dir)
-        self.config_file = os.path.join(self.config_dir, config_file_name)
-        if not os.path.isfile(self.config_file):
-            raise Exception("Config file '%s' not found" % self.config_file)
-        self.channel_config_dir = os.path.join(self.config_dir, "config.d")
+            raise Exception(f"Config dir '{self.config_dir}' not found")
+        self.channel_config_dir = os.getenv("CHANNEL_CONFIG_PATH",
+                                            os.path.join(self.config_dir, "config.d"))
+        if not os.path.isdir(self.channel_config_dir):
+            raise Exception(f"Channel config dir '{self.channel_config_dir}' not found")
         self.global_config = {}
         self.channels = {}
         self.config = {}
         self.logger = None
         self.last_update_time = 0.0
-
-    def check_keys(self, channel_conf_dict):
-        """
-        check that channel config has mandatory keys
-        :type data: :obj:`dict`
-        """
-        channel_keys = set(channel_conf_dict.keys())
-        diff = MANDATORY_CHANNEL_KEYS - channel_keys
-        if diff:
-            raise RuntimeError(f"channel is missing one or more mandatory keys {list(diff)} ")
-        for name in MANDATORY_CHANNEL_KEYS:
-            conf = channel_conf_dict[name]
-            for module_name, module_conf in conf.items():
-                try:
-                    module_keys = set(module_conf.keys())
-                except Exception as msg:
-                    raise RuntimeError(f"{name} module {module_name} is not a dictionary, {msg}")
-                diff = MANDATORY_MODULE_KEYS - module_keys
-                if diff:
-                    missing_keys = str(list(diff))
-                    raise RuntimeError(f"{name} module {module_name} is missing one or more mandatory keys {missing_keys} ")
-
-    def validate_channel(self, channel):
-        """
-        Validate channels
-        :type channel: :obj:`dict`
-        """
-        self.check_keys(channel)
-        """
-        sources
-        """
-        sources = channel.get('sources')
-        all_produces = set()
-        for sname, conf in sources.items():
-            my_module = importlib.import_module(conf.get('module'))
-            try:
-                produces = getattr(my_module, 'PRODUCES')
-                all_produces |= set(produces)
-            except AttributeError:
-                raise RuntimeError(f"source module {sname} does not have required PRODUCES list")
-
-        all_consumes = set()
-
-        """
-        transforms
-        """
-        transforms = channel.get('transforms')
-        transform_map = {}
-
-        for tname, conf in transforms.items():
-            my_module = importlib.import_module(conf.get('module'))
-            try:
-                consumes = set(getattr(my_module, 'CONSUMES'))
-                produces = set(getattr(my_module, 'PRODUCES'))
-                all_consumes |= consumes
-                all_produces |= produces
-                transform_map[tname] = {'consumes': consumes,
-                                        'produces': produces}
-            except AttributeError as msg:
-                raise RuntimeError(f"transform module {tname} does not have required lists {msg}")
-
-        if not all_consumes.issubset(all_produces):
-            extra_keys = list(all_consumes - all_produces)
-            raise RuntimeError(f"consumes are not subset of produce, extra keys {extra_keys}")
-
-        """
-        graph contains pairs of modules and lists of modules that depends on
-        this module, e.g.:
-        { "A" : [B,C,D],
-        "D" : [C,B] }
-        """
-
-        graph = {}
-        for i in range(len(transform_map)):
-            k1 = list(transform_map.keys())[i]
-            c1 = set(transform_map[k1].get('consumes', []))
-            p1 = set(transform_map[k1].get('produces', []))
-            added = False
-            for j in range(i + 1, len(transform_map)):
-                k2 = list(transform_map.keys())[j]
-                c2 = set(transform_map[k2].get('consumes', []))
-                p2 = set(transform_map[k2].get('produces', []))
-                if c2 & p1:
-                    added = True
-                    graph.setdefault(k1, []).append(k2)
-                if p2 & c1:
-                    added = True
-                    graph.setdefault(k2, []).append(k1)
-            if not added:
-                graph.setdefault(k1, [])
-
-        """
-        sort modules using topological sort
-        sorted_modules are transform modules in order of execution
-        """
-        sorted_modules, cyclic_modules = tsort.tsort(graph)
-
-        if cyclic_modules:
-            raise RuntimeError(f"cyclic dependency detected for modules {list(cyclic_modules)}")
 
     def get_produces(self, channel_config):
         produces = {}
@@ -159,65 +167,25 @@ class ConfigManager():
                     pass
         return produces
 
-    def reload(self):
+    def reload(self, config_file_name=CONFIG_FILE_NAME):
         old_global_config = copy.deepcopy(self.global_config)
         old_config = copy.deepcopy(self.config)
         try:
-            self.load()
+            self.load(config_file_name)
         except Exception:
             self.global_config = copy.deepcopy(old_global_config)
             self.config = copy.deepcopy(old_config)
             raise RuntimeError
 
-    def load(self):
-        self.last_update_time = os.stat(self.config_file).st_mtime
-        self.global_config = _config_from_file(self.config_file)
-
+    def load(self, config_file_name=CONFIG_FILE_NAME):
+        config_file = os.path.join(self.config_dir, config_file_name)
+        if not os.path.isfile(config_file):
+            raise Exception(f"Config file '{config_file}' not found")
+        self.last_update_time = os.stat(config_file).st_mtime
+        self.global_config = _config_from_file(config_file)
         if not self.logger:
-            if 'logger' not in self.global_config:
-                raise RuntimeError("No logger configuration has been specified.")
-            try:
-                logger_config = self.global_config['logger']
-                de_logger.set_logging(log_file_name=logger_config['log_file'],
-                                      max_file_size=logger_config['max_file_size'],
-                                      max_backup_count=logger_config['max_backup_count'],
-                                      log_level=logger_config.get('log_level', 'WARNING'))
-                self.logger = de_logger.get_logger()
-            except Exception as msg:
-                raise RuntimeError(f"Failed to create log: {msg}")
-
-        """
-        load channels
-        """
-
-        for direntry in os.listdir(self.channel_config_dir):
-            if not direntry.endswith(".conf"):
-                continue
-            name = direntry.split('.')[0]
-            channel_conf = os.path.join(self.channel_config_dir, direntry)
-            try:
-                with open(os.path.abspath(channel_conf), "r") as f:
-                    code = "self.channels[name]=" + "".join(f.readlines())
-                    try:
-                        exec(code)
-                    except Exception as msg:
-                        self.logger.error(f"Channel configuration file {channel_conf} \
-                                            contains error {msg}, SKIPPING")
-                        continue
-            except Exception as msg:
-                self.logger.error(f"Failed to open channel configuration file {channel_conf} \
-                                  contains error {msg}, SKIPPING")
-
-            """
-            check that channel configuration contains necessary keys
-            if keys are missing channel is removed and error is printed
-            """
-            try:
-                self.validate_channel(self.channels[name])
-            except Exception as msg:
-                self.logger.error(f"{name} {msg}, REMOVING the channel")
-                del self.channels[name]
-                continue
+            self.logger = _make_logger(self.global_config)
+        self._load_channels()
 
     def is_updated(self):
         return
@@ -228,11 +196,39 @@ class ConfigManager():
     def get_global_config(self):
         return self.global_config
 
-    """
-    Factory method:  create instance of dynamically loaded module
-    """
+    def _load_channels(self):
+        for entry in os.scandir(self.channel_config_dir):
+            name, path = entry.name, entry.path
+            if not name.endswith(".conf"):
+                continue
+            _verify_not_empty(path)
+            try:
+                with open(path) as f:
+                    try:
+                        self.channels[name] = eval(f.read())
+                    except Exception as msg:
+                        self.logger.error(f"Channel configuration file {path} "
+                                          f"contains error\n-> {msg}\nSKIPPING channel")
+                        continue
+            except Exception as msg:
+                self.logger.error(f"Failed to open channel configuration file {path} "
+                                  f"contains error\n-> {msg}\nSKIPPING channel")
+
+            # Verify channel configuration contains necessary keys.
+            # If keys are missing, the channel is removed and an error
+            # message is logged.
+            try:
+                _validate_channel(self.channels[name])
+            except Exception as msg:
+                self.logger.error(f"{name} {msg}, REMOVING the channel")
+                del self.channels[name]
+                continue
+
     @staticmethod
     def create(module_name, class_name, parameters):
+        """
+        Factory method:  create instance of dynamically loaded module
+        """
         my_module = importlib.import_module(module_name)
         clazz = getattr(my_module, class_name)
         instance = clazz(parameters)
--- framework/configmanager/tests/test_configmanager.py
+++ framework/configmanager/tests/test_configmanager.py
@@ -1,34 +1,63 @@
 import os
 import pytest
+import re
 
 from decisionengine.framework.configmanager import ConfigManager
 
-@pytest.fixture(autouse=True)
-def env_setup(monkeypatch):
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    monkeypatch.setenv('CONFIG_PATH',
-                       os.path.join(this_dir, 'de'))
+_this_dir = os.path.dirname(os.path.abspath(__file__))
 
-def load(filename):
-    manager = ConfigManager.ConfigManager(filename)
-    manager.load()
+@pytest.fixture()
+def load(monkeypatch):
+    def _call(filename, relative_channel_config_dir=None):
+        monkeypatch.setenv('CONFIG_PATH', os.path.join(_this_dir, 'de'))
+        if relative_channel_config_dir is None:
+            monkeypatch.setenv('CHANNEL_CONFIG_PATH',
+                               os.path.join(_this_dir, 'channels/no_config_files'))
+        else:
+            monkeypatch.setenv('CHANNEL_CONFIG_PATH',
+                               os.path.join(_this_dir, relative_channel_config_dir))
+        manager = ConfigManager.ConfigManager()
+        manager.load(filename)
+    return _call
 
-def test_empty_config():
+
+#-------------------------------------------------
+def test_empty_config(load):
     with pytest.raises(RuntimeError) as e:
         load('empty.conf')
     assert e.match('Empty configuration file .*/empty\\.conf')
 
-def test_wrong_type():
+def test_wrong_type(load):
     with pytest.raises(RuntimeError) as e:
         load('wrong_type.conf')
     assert e.match('The configuration file must be a Python dictionary')
 
-def test_empty_dict():
+def test_empty_dict(load):
     with pytest.raises(RuntimeError) as e:
         load('empty_dict.conf')
     assert e.match('No logger configuration has been specified')
 
-def test_empty_dict_with_leading_comment():
+def test_empty_dict_with_leading_comment(load):
     with pytest.raises(RuntimeError) as e:
         load('empty_dict_with_leading_comment.conf')
     assert e.match('No logger configuration has been specified')
+
+# -------------------------------------------------
+def test_minimal_python(load):
+    load('minimal_python.conf')
+
+# -------------------------------------------------
+def test_channel_no_config_files(load):
+    load('minimal_python.conf', 'channels/no_config_files')
+
+def test_channel_empty_config(load):
+    with pytest.raises(RuntimeError) as e:
+        load('minimal_python.conf', 'channels/empty_config')
+    assert e.match('Empty configuration file .*\\.conf')
+
+def test_channel_empty_dictionary(load, caplog):
+    load('minimal_python.conf', 'channels/empty_dictionary')
+    assert re.search("channel is missing one or more mandatory keys", caplog.text)
+
+def test_channel_no_modules(load):
+    load('minimal_python.conf', 'channels/no_modules')
