ebee054b2121376658d57fbdb3529d2fe445506f
Kyle Knoepfel <knoepfel@fnal.gov>
2020-06-26 13:18:07 -0500

    Explain what the different tests test.

--- framework/configmanager/tests/test_configmanager.py
+++ framework/configmanager/tests/test_configmanager.py
@@ -21,7 +21,9 @@ def load(monkeypatch):
     return _call
 
 
-#-------------------------------------------------
+# --------------------------------------------------------------------
+# These tests demonstrate failure modes when reading a DE
+# applicaation configuration file.
 def test_empty_config(load):
     with pytest.raises(RuntimeError) as e:
         load('empty.conf')
@@ -32,6 +34,10 @@ def test_wrong_type(load):
         load('wrong_type.conf')
     assert e.match('The supplied configuration must be a valid Jsonnet/JSON document')
 
+# --------------------------------------------------------------------
+# These tests yield valid DE configuration structures. However, the
+# configurations are missing a logger configuration, which is the next
+# failure mode after reading the configuration files.
 def test_empty_dict(load):
     with pytest.raises(RuntimeError) as e:
         load('empty_dict.conf')
@@ -42,8 +48,10 @@ def test_empty_dict_with_leading_comment(load):
         load('empty_dict_with_leading_comment.conf')
     assert e.match('No logger configuration has been specified')
 
-#--------------------------------
-
+# --------------------------------------------------------------------
+# These tests validate well-formed DE configurations, but they also
+# check that the appropriate warning messages are emitted regarding
+# Python support for a Jsonnet configuration system.
 def test_minimal_python(load, capsys):
     load('minimal_python.conf')
     stdout, stderr = capsys.readouterr()
@@ -58,7 +66,9 @@ def test_minimal_jsonnet_wrong_extension(load, capsys):
     expected = r"Please rename '.*/minimal_jsonnet\.conf' to '.*/minimal_jsonnet\.jsonnet'"
     assert re.match(expected, stderr)
 
-# -------------------------------------------------
+# --------------------------------------------------------------------
+# These tests verify expected behavior for channel (not DE)
+# configurations.
 def test_channel_no_config_files(load):
     load('minimal_python.conf', 'channels/no_config_files')
 
