2bbeb0ddf910d88f64896c4046ea9e6fdb04d49d
Pat Riehecky <riehecky@fnal.gov>
2020-09-02 16:05:44 -0500

    Merge 'runtime' tests into running channel tests
    Also fixes #148

--- framework/tests/test_channel.py
+++ framework/tests/test_channel.py
@@ -7,13 +7,17 @@ import pytest
 from pytest_postgresql import factories
 
 import decisionengine.framework.engine.de_client as de_client
-from decisionengine.framework.configmanager.ConfigManager import ConfigManager
-from decisionengine.framework.engine.DecisionEngine import DecisionEngine
+from decisionengine.framework.engine.DecisionEngine import _get_de_conf_manager, _start_de_server
 from decisionengine.framework.util.sockets import get_random_port
 
 _CWD = os.path.dirname(os.path.abspath(__file__))
 _DDL_FILE = "../dataspace/datasources/postgresql.sql"
 _CONFIG_PATH = os.path.join(_CWD, "etc/decisionengine")
+_LOG_LEVELS = ['NOTSET', 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+# Not all test hosts are IPv6, generally IPv4 works fine
+#  some test hosts use IPv6 for localhost by default, even when not configured!
+#  Python XML RPC Socket server is IPv4 only right now.
+_HOST = '127.0.0.1'
 
 postgresql_my_proc = factories.postgresql_proc(port=None)
 postgresql = factories.postgresql("postgresql_my_proc",
@@ -40,12 +44,12 @@ class Worker(multiprocessing.Process):
 
     def run(self):
         os.environ["CONFIG_PATH"] = _CONFIG_PATH
-        conf_manager = ConfigManager()
-        conf_manager.load()
+        conf_manager = _get_de_conf_manager([])
+
         #
         # Override config for testing
         #
-        conf_manager.global_config['server_address'] = ['localhost', self.port]
+        conf_manager.global_config['server_address'] = [_HOST, self.port]
         conf_manager.global_config['dataspace']['datasource']['config']['port'] \
             = self.db_parameters['port']
         conf_manager.global_config['dataspace']['datasource']['config']['user'] \
@@ -53,13 +57,7 @@ class Worker(multiprocessing.Process):
         conf_manager.global_config['dataspace']['datasource']['config']['database'] \
             = self.db_parameters['dbname']
 
-        global_config = conf_manager.get_global_config()
-        server_address = tuple(global_config.get('server_address'))
-        server = DecisionEngine(conf_manager, server_address)
-        server.reaper_start(delay=global_config['dataspace'].
-                            get('reaper_start_delay_seconds', 1818))
-        server.start_channels()
-        server.serve_forever()
+        _start_de_server(conf_manager)
 
 
 @pytest.mark.usefixtures("fixtures")
@@ -71,9 +69,9 @@ class TestChannel(unittest.TestCase):
                              self.port)
         self.worker.start()
         #
-        # config used by this test uses 1 second schedule, run few cycles
+        # config used by this test uses 1 second schedule, make sure one completes
         #
-        time.sleep(5)
+        time.sleep(1)
 
     def tearDown(self):
         try:
@@ -86,16 +84,68 @@ class TestChannel(unittest.TestCase):
                 self.worker.terminate()
 
     def de_client_request(self, *args):
-        return de_client.main(["--host=localhost",
+        return de_client.main(["--host",
+                               _HOST,
                                "--port",
                                str(self.port),
                                *args])
 
     def test_client_can_get_de_server_status(self):
+        # make sure channels have a chance to get into state
+        time.sleep(5)
         output = self.de_client_request("--status")
-        self.assertIn("STEADY",
+        self.assertIn("STEADY", output, msg="Channel not in STEADY state")
+
+    def test_client_can_get_de_server_show_config(self):
+        output = self.de_client_request('--show-config')
+        self.assertNotEqual('{}', output, msg="DE didn't share channel configs")
+
+    def test_client_can_get_de_server_reload_config(self):
+        output = self.de_client_request('--reload-config')
+        self.assertEqual('OK', output, msg="DE didn't say OK")
+
+    def test_client_can_get_de_server_reaper_status(self):
+        output = self.de_client_request('--reaper-status')
+        self.assertIn('reaper:', output, msg="Couldn't find reaper section")
+        self.assertIn('state:', output, msg="Couldn't find reaper state")
+        self.assertIn('retention_interval:', output, msg="Couldn't find reaper interval")
+
+    def test_client_can_get_de_server_reaper_stop(self):
+        output = self.de_client_request('--reaper-stop')
+        self.assertEqual('OK', output, msg="DE didn't say OK")
+
+        # get status to be sure
+        output = self.de_client_request('--reaper-status')
+        self.assertIn('reaper:', output, msg="Couldn't find reaper section")
+        self.assertIn('state:', output, msg="Couldn't find reaper state")
+        self.assertIn('State.STOPPED', output, msg="reaper state incorrect")
+
+    def test_client_can_get_de_server_reaper_start_delay(self):
+        # make sure the reaper is stopped first
+        self.de_client_request('--reaper-stop')
+
+        output = self.de_client_request('--reaper-start', '--reaper-start-delay-secs=90')
+        self.assertEqual('OK', output, msg="DE didn't say OK")
+
+        # get status to be sure
+        output = self.de_client_request('--reaper-status')
+        self.assertIn('reaper:', output, msg="Couldn't find reaper section")
+        self.assertIn('state:', output, msg="Couldn't find reaper state")
+        self.assertIn('State.STARTING', output, msg="reaper state incorrect")
+
+    def test_client_can_get_de_server_show_logger_level(self):
+        output = self.de_client_request('--print-engine-loglevel')
+        self.assertIn(output, _LOG_LEVELS, msg="DE didn't give a valid log level")
+
+    def test_client_can_get_de_server_show_channel_logger_level(self):
+        output = self.de_client_request('--get-channel-loglevel=UNITTEST')
+        self.assertIn(output, _LOG_LEVELS, msg="DE didn't get channel logger level")
+
+    def test_global_channel_log_level_in_config(self):
+        output = self.de_client_request('--show-de-config')
+        self.assertIn('global_channel_log_level',
                       output,
-                      msg="Channel not in STEADY state")
+                      msg="Global channel log level not set in config file. Default value is INFO.")
 
 
 if __name__ == "__main__":
