ea9a600c39902a0f92e002e6fd3c452ecafd8e0f
Lisa Goodenough <goodenou@fnal.gov>
2020-09-21 13:33:36 -0500

    Adding log rotation by time.
    Changes to add ability to rotate the log files according to a time interval. The choice for whether to rotate log files according to time or file size is made in the config file (decision_engine.jsonnet) in the "file_rotate_by" parameter. The config file includes parameters for the unit of time and the time interval. The time interval is the same for both the DE log file and the channel log files, just as the size is.
    
    RB : https://fermicloud140.fnal.gov/reviews/r/212/

--- framework/engine/DecisionEngine.py
+++ framework/engine/DecisionEngine.py
@@ -51,7 +51,7 @@ class Worker(multiprocessing.Process):
         if logger_rotate_by == "size":
             file_handler = logging.handlers.RotatingFileHandler(os.path.join(
                                                                 os.path.dirname(
-                                                                  self.logger_config["log_file"]),
+                                                                    self.logger_config["log_file"]),
                                                                 self.task_manager.name + ".log"),
                                                                 maxBytes=self.logger_config.get("max_file_size",
                                                                 200 * 1000000),
@@ -60,7 +60,7 @@ class Worker(multiprocessing.Process):
         else:
             file_handler = logging.handlers.TimedRotatingFileHandler(os.path.join(
                                                                      os.path.dirname(
-                                                                       self.logger_config["log_file"]),
+                                                                         self.logger_config["log_file"]),
                                                                      self.task_manager.name + ".log"),
                                                                      when=self.logger_config.get("rotation_time_unit", 'D'),
                                                                      interval=self.logger_config.get("rotation_time_interval", '1'))
