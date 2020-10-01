8c94b7b4f5fa7f7a45e9128429cbeaf0b0b68ab4
Kyle Knoepfel <knoepfel@fnal.gov>
2020-10-01 16:26:59 -0500

    Mock data-block.

--- framework/taskmanager/tests/test_task_manager.py
+++ framework/taskmanager/tests/test_task_manager.py
@@ -1,17 +1,41 @@
+from collections import UserDict
+import mock
+import os
+import multiprocessing
+import pytest
+import threading
+import time
+
 from decisionengine.framework.config.ValidConfig import ValidConfig
 import decisionengine.framework.config.policies as policies
 from decisionengine.framework.taskmanager.TaskManager import TaskManager, State
-
-import os
-import threading
-import time
-import uuid
+import decisionengine.framework.dataspace.datablock
 
 _CWD = os.path.dirname(os.path.abspath(__file__))
 _CONFIG_PATH = os.path.join(_CWD, "../../tests/etc/decisionengine")
 _CHANNEL_CONFIG_DIR = os.path.join(_CWD, 'channels')
 
 _global_config = ValidConfig(policies.global_config_file(_CONFIG_PATH))
+
+class MockDataBlock(UserDict):
+    def __init__(self, products={}):
+        self.lock = threading.Lock()
+        self.taskmanager_id = None
+        self.generation_id = 1
+        self.data = products
+
+    def duplicate(self):
+        return MockDataBlock(self.data)
+
+    def put(self, key, product, header, metadata=None):
+        self.data[key] = product
+
+@pytest.fixture
+def mock_data_block():
+    with mock.patch('decisionengine.framework.dataspace.datablock.DataBlock') as mock_data_block:
+        mock_data_block.return_value = MockDataBlock()
+        yield
+
 
 def channel_config(name):
     return ValidConfig(os.path.join(_CHANNEL_CONFIG_DIR, name + '.jsonnet'))
@@ -22,28 +46,29 @@ def task_manager_for(name):
 class RunChannel():
     def __init__(self, name):
         self._tm = task_manager_for(name)
-        self._thread = threading.Thread(target=self._tm.run)
+        self._process = multiprocessing.Process(target=self._tm.run)
 
     def __enter__(self):
-        self._thread.start()
+        self._process.start()
         return self._tm
 
     def __exit__(self, type, value, traceback):
-        self._thread.join()
+        self._process.join()
+        if type:
+            return False
 
-
-def test_task_manager_construction():
+def test_task_manager_construction(mock_data_block):
     task_manager = task_manager_for('test_channel')
     assert task_manager.get_state() == State.BOOT
 
-def test_take_task_manager_offline():
+def test_take_task_manager_offline(mock_data_block):
     with RunChannel('test_channel') as task_manager:
-        time.sleep(3)
+        time.sleep(2)
         assert task_manager.get_state() == State.STEADY
         task_manager._take_offline(None)
         assert task_manager.get_state() == State.OFFLINE
 
-def test_failing_publisher():
+def test_failing_publisher(mock_data_block):
     with RunChannel('failing_publisher') as task_manager:
-        time.sleep(3)
+        time.sleep(2)
         assert task_manager.get_state() == State.OFFLINE
