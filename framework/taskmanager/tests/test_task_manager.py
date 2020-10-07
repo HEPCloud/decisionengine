fbe7616265244aabf08d3718df044b0d3cbdfe9d
Kyle Knoepfel <knoepfel@fnal.gov>
2020-10-07 12:33:03 -0500

    Test task manager (#196)
    * First task manager tests.
    * Better encapsulate task-manager innards.
    * Test failing publisher.
    * Mock data-block.
    * Handle error conditions in publishers and logic engines.
    * Move fixture into dedicated location.
    * Expand test suite to include restarting channel.
    
    RB : https://fermicloud140.fnal.gov/reviews/r/244/

---
+++ framework/taskmanager/tests/test_task_manager.py
@@ -0,0 +1,57 @@
+import multiprocessing
+import os
+import time
+
+import decisionengine.framework.config.policies as policies
+from decisionengine.framework.config.ValidConfig import ValidConfig
+from decisionengine.framework.dataspace.datasources.tests.fixtures import mock_data_block  # noqa: F401
+from decisionengine.framework.taskmanager.TaskManager import State
+from decisionengine.framework.taskmanager.TaskManager import TaskManager
+
+_CWD = os.path.dirname(os.path.abspath(__file__))
+_CONFIG_PATH = os.path.join(_CWD, "../../tests/etc/decisionengine")
+_CHANNEL_CONFIG_DIR = os.path.join(_CWD, 'channels')
+
+_global_config = ValidConfig(policies.global_config_file(_CONFIG_PATH))
+
+
+def channel_config(name):
+    return ValidConfig(os.path.join(_CHANNEL_CONFIG_DIR, name + '.jsonnet'))
+
+
+def task_manager_for(name):
+    return TaskManager(name, 1, channel_config(name), _global_config)
+
+
+class RunChannel:
+    def __init__(self, name):
+        self._tm = task_manager_for(name)
+        self._process = multiprocessing.Process(target=self._tm.run)
+
+    def __enter__(self):
+        self._process.start()
+        return self._tm
+
+    def __exit__(self, type, value, traceback):
+        self._process.join()
+        if type:
+            return False
+
+
+def test_task_manager_construction(mock_data_block):  # noqa: F811
+    task_manager = task_manager_for('test_channel')
+    assert task_manager.get_state() == State.BOOT
+
+
+def test_take_task_manager_offline(mock_data_block):  # noqa: F811
+    with RunChannel('test_channel') as task_manager:
+        time.sleep(2)
+        assert task_manager.get_state() == State.STEADY
+        task_manager._take_offline(None)
+        assert task_manager.get_state() == State.OFFLINE
+
+
+def test_failing_publisher(mock_data_block):  # noqa: F811
+    with RunChannel('failing_publisher') as task_manager:
+        time.sleep(2)
+        assert task_manager.get_state() == State.OFFLINE
---
+++ framework/tests/test_restart_channel.py
@@ -0,0 +1,61 @@
+import multiprocessing
+import os
+import pathlib
+import re
+import time
+
+import pytest
+
+import decisionengine.framework.engine.de_client as de_client
+from decisionengine.framework.dataspace.datasources.tests.fixtures import mock_data_block  # noqa: F401
+from decisionengine.framework.engine.DecisionEngine import _get_de_conf_manager
+from decisionengine.framework.engine.DecisionEngine import _start_de_server
+from decisionengine.framework.engine.DecisionEngine import parse_program_options
+from decisionengine.framework.util.sockets import get_random_port
+
+_this_dir = pathlib.Path(__file__).parent.resolve()
+_CONFIG_PATH = os.path.join(_this_dir, "etc/decisionengine")
+_CHANNEL_CONFIG_PATH = os.path.join(_CONFIG_PATH, 'config.d')
+
+_port = get_random_port()
+
+
+def run_server():
+    global_config, channel_config_handler = _get_de_conf_manager(_CONFIG_PATH,
+                                                                 _CHANNEL_CONFIG_PATH,
+                                                                 parse_program_options([f'--port={_port}']))
+    _start_de_server(global_config, channel_config_handler)
+
+
+def de_client_request(*args):
+    return de_client.main([f"--port={_port}", *args])
+
+
+@pytest.fixture
+def deserver_mock_data_block(mock_data_block):  # noqa: F811
+    worker = multiprocessing.Process(target=run_server)
+    worker.start()
+    time.sleep(2)  # Make sure channels have enough time to start
+    yield
+    de_client_request("--stop")
+    worker.terminate()
+
+
+# Because pytest will reorder the tests based on the method name,
+# we implement the whole 'workflow' as one unit test.
+def test_restart_channel(deserver_mock_data_block):
+    # Verify that nothing is active
+    output = de_client_request("--status")
+    assert re.search('test_channel.*state = STEADY', output)
+
+    # Take channel offline
+    output = de_client_request('--stop-channel', 'test_channel')
+    assert output == 'OK'
+
+    # Verify no channels are active
+    output = de_client_request("--status")
+    assert "No channels are currently active" in output
+
+    # Take channel offline
+    output = de_client_request('--start-channel', 'test_channel')
+    assert output == 'OK'
