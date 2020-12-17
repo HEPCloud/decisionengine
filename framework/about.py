70b6c9b8b4208e048611e2040fd07f3dd21b58ec
Pat Riehecky <riehecky@fnal.gov>
2020-12-17 14:34:54 -0600

    Add PEP-0396 metadata

---
+++ framework/about.py
@@ -0,0 +1,10 @@
+'''
+    PEP-0396 provides instructions for providing module versions
+    While we are at it, add a few other useful bits
+'''
+__version__ = '1.5.0'
+__title__ = 'decisionengine'
+__description__ = 'The HEPCloud Decision Engine Framework'
+__author__ = 'Fermilab'
+__license__ = 'Fermitools Software Legal Information (Modified BSD License)'
+__url__ = 'http://hepcloud.fnal.gov'
