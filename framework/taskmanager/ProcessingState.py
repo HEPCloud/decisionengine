7df39b2dbd666a03f4c776aaee7eebda11e8e359
Lisa Goodenough <goodenou@fnal.gov>
2021-02-12 16:15:23 -0600

    logging and error handling enhancements to taskmanager directory
     https://fermicloud140.fnal.gov/reviews/r/326/

--- framework/taskmanager/ProcessingState.py
+++ framework/taskmanager/ProcessingState.py
@@ -15,7 +15,7 @@ or exited, respectively.
 
 import enum
 import multiprocessing
-
+import logging
 
 class State(enum.Enum):
     BOOT = 0
@@ -31,24 +31,37 @@ class ProcessingState:
         allowed_state = State(state)
         self._cv = multiprocessing.Condition()
         self._state = multiprocessing.Value('i', allowed_state.value)
+        self.logger = logging.getLogger()
 
     def get(self):
-        value = None
-        with self._state.get_lock():
-            value = self._state.value
-        return State(value)
+        try:
+            value = None
+            with self._state.get_lock():
+                value = self._state.value
+            return State(value)
+        except Exception:
+            self.logger.exception("Unexpected error!")
+            raise
 
     def set(self, state):
-        if not isinstance(state, State):
-            raise RuntimeError('Supplied value is not a State variable.')
-        with self._cv, self._state.get_lock():
-            self._state.value = state.value
-            self._cv.notify()
+        try:
+            if not isinstance(state, State):
+                raise RuntimeError("Supplied value is not a State variable.")
+            with self._cv, self._state.get_lock():
+                self._state.value = state.value
+                self._cv.notify()
+        except Exception:
+            self.logger.exception("Unexpected error!")
+            raise
 
     def has_value(self, state):
-        if not isinstance(state, State):
-            raise RuntimeError('Supplied value is not a State variable.')
-        return self.get() == state
+        try:
+            if not isinstance(state, State):
+                raise RuntimeError("Supplied value is not a State variable.")
+            return self.get() == state
+        except Exception:
+            self.logger.exception("Unexpected error!")
+            raise
 
     def wait_until(self, state):
         with self._cv:
