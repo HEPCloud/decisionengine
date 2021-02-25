cf9c840cec1c11a4e32daea9c67ce3fc5671859d
Kyle Knoepfel <knoepfel@fnal.gov>
2021-02-25 10:31:30 -0600

    Allow conversion from errors to false values in logic-engine expressions.

--- src/decisionengine/framework/logicengine/BooleanExpression.py
+++ src/decisionengine/framework/logicengine/BooleanExpression.py
@@ -12,14 +12,16 @@
 import ast
 import logging
 
-# If support for direct use of numpy and pandas functions becomes
-# wanted, then uncomment the following two import statements, and
-# switch the definition of facts_globals to the commented-out
-# version.#
+# If support for direct use of numpy and pandas functions is desired,
+# import the numpy and pandas modules and adjust the facts_globals:
+#   facts_globals.update(np=np, pd=pd)
 
-facts_globals = {}
-# facts_globals = {"np": np, "pd": pd}
 
+def fail_on_error(expr):
+    pass
+
+
+facts_globals = {'fail_on_error': fail_on_error}
 
 class LogicError(TypeError):
     pass
@@ -49,6 +51,7 @@ class BooleanExpression:
         syntax_tree = ast.parse(expr, source, mode)
         all_names = [n.id for n in ast.walk(syntax_tree) if isinstance(n, ast.Name)]
         func_names = [function_name_from_call(n) for n in ast.walk(syntax_tree) if isinstance(n, ast.Call)]
+        self.fail_on_error = func_names[0] == 'fail_on_error' if func_names else False
 
         self.required_names = list(set(all_names) - set(func_names))
         self.expr = compile(syntax_tree, source, mode)
@@ -57,7 +60,14 @@ class BooleanExpression:
         """Return the evaluated Boolen value of this expression in the context
         of the given data 'd'."""
         logging.getLogger().debug("calling NamedFact::evaluate()")
-        return bool(eval(self.expr, facts_globals, d))
+        try:
+            return bool(eval(self.expr, facts_globals, d))
+        except Exception:
+            if self.fail_on_error:
+                logging.getLogger().exception("The following exception was suppressed, and the "
+                                              "Boolean expression will evaluate to False.")
+                return False
+            raise
 
     def __str__(self):
         return f"{self.expr_str}"
