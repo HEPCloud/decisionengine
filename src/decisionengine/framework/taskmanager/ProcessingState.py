10384a8cf3167bbadc0bfea08291c9eeb20cb01c
Pat Riehecky <riehecky@fnal.gov>
2021-03-11 14:17:33 -0600

    Move reaper into its own place and reuse state logic
    RB: https://fermicloud140.fnal.gov/reviews/r/370/

--- src/decisionengine/framework/taskmanager/ProcessingState.py
+++ src/decisionengine/framework/taskmanager/ProcessingState.py
@@ -2,6 +2,8 @@
 The ProcessingState class can represent any of the following task-manager states:
 
   BOOT
+  IDLE
+  ACTIVE
   STEADY
   OFFLINE
   SHUTTINGDOWN
@@ -11,6 +13,10 @@ The ProcessingState class can represent any of the following task-manager states
 In addition, the class supports 'wait_until(state)' and 'wait_while(state)' methods,
 which, when called from a different process, block until the state has been entered
 or exited, respectively.
+
+The 'RUNNING_CONDITIONS' list is a list of states that a thread may have if it is started/starting.
+The 'STOPPING_CONDITIONS' list is a list of states that a thread may have if it is stopped/stopping.
+The 'INACTIVE_CONDITIONS' list is a list of states that a thread may have when it is not active
 '''
 
 import enum
@@ -19,21 +25,48 @@ import logging
 
 class State(enum.Enum):
     BOOT = 0
-    STEADY = 1
-    SHUTTINGDOWN = 2
-    SHUTDOWN = 3
-    OFFLINE = 4
-    ERROR = 5
+    IDLE = 1
+    ACTIVE = 2
+    STEADY = 3
+    SHUTTINGDOWN = 4
+    SHUTDOWN = 5
+    OFFLINE = 6
+    ERROR = 7
+
+
+RUNNING_CONDITIONS = (State.BOOT, State.IDLE, State.ACTIVE, State.STEADY)
+STOPPING_CONDITIONS = (State.SHUTTINGDOWN, State.SHUTDOWN, State.OFFLINE, State.ERROR)
+INACTIVE_CONDITIONS = (State.OFFLINE, State.ERROR)
 
 
 class ProcessingState:
+    """
+    This object tracks the state of a process.
+
+    A number of convience wrappers are provided.
+
+    Additionally you may use the `.lock` attribute for `with` block
+    to lock the state during specific operations.
+    """
     def __init__(self, state=State.BOOT):
         allowed_state = State(state)
         self._cv = multiprocessing.Condition()
+        self._lock = multiprocessing.RLock()
         self._state = multiprocessing.Value('i', allowed_state.value)
         self.logger = logging.getLogger()
 
+    @property
+    def lock(self):
+        return self._lock
+
+    @lock.setter
+    def lock(self, value):
+        raise ValueError("You may not redefine the ProcessingState lock")
+
     def get(self):
+        """
+        This function is a minimally locking check to fetch the state.
+        """
         try:
             value = None
             with self._state.get_lock():
@@ -44,35 +77,49 @@ class ProcessingState:
             raise
 
     def set(self, state):
+        """
+        This function will lock (and possibly block) to ensure a consistent
+        change to the state value.
+
+        This function can be blocked using the `.lock` to force state
+        sync between threads if need be.
+        """
         try:
             if not isinstance(state, State):
                 raise RuntimeError("Supplied value is not a State variable.")
-            with self._cv, self._state.get_lock():
-                self._state.value = state.value
-                self._cv.notify()
+            with self.lock:  # don't hold other locks if we can't get this one
+                with self._cv, self._state.get_lock():
+                    self._state.value = state.value
+                    self._cv.notify()
         except Exception:
             self.logger.exception("Unexpected error!")
             raise
 
     def has_value(self, state):
         try:
-            if not isinstance(state, State):
-                raise RuntimeError("Supplied value is not a State variable.")
-            return self.get() == state
+            if not isinstance(state, State) and not isinstance(state, (list, tuple)):
+                raise RuntimeError("Supplied value is not a State variable or list of State values.")
+            if isinstance(state, (list, tuple)):
+                return True in map(self.has_value, state)
+            else:
+                return self.get() == state
         except Exception:
             self.logger.exception("Unexpected error!")
             raise
 
-    def wait_until(self, state):
+    def wait_until(self, state, timeout=None):
         with self._cv:
-            self._cv.wait_for(lambda: self.has_value(state))
+            self._cv.wait_for(lambda: self.has_value(state), timeout)
 
-    def wait_while(self, state):
+    def wait_while(self, state, timeout=None):
         with self._cv:
-            self._cv.wait_for(lambda: not self.has_value(state))
+            self._cv.wait_for(lambda: not self.has_value(state), timeout)
+
+    def probably_running(self):
+        return self.has_value(RUNNING_CONDITIONS)
 
     def should_stop(self):
-        return self.get().value > State.STEADY.value
+        return self.has_value(STOPPING_CONDITIONS)
 
     def inactive(self):
-        return self.get().value > State.SHUTTINGDOWN.value
+        return self.has_value(INACTIVE_CONDITIONS)
--- src/decisionengine/framework/taskmanager/tests/test_processing_state.py
+++ src/decisionengine/framework/taskmanager/tests/test_processing_state.py
@@ -5,6 +5,9 @@ import pytest
 
 from decisionengine.framework.taskmanager.ProcessingState import State
 from decisionengine.framework.taskmanager.ProcessingState import ProcessingState
+from decisionengine.framework.taskmanager.ProcessingState import RUNNING_CONDITIONS
+from decisionengine.framework.taskmanager.ProcessingState import STOPPING_CONDITIONS
+from decisionengine.framework.taskmanager.ProcessingState import INACTIVE_CONDITIONS
 
 
 class Worker(multiprocessing.Process):
@@ -23,17 +26,34 @@ def test_shared_state_construction():
     state = ProcessingState(State.STEADY)
     assert state.has_value(State.STEADY)
 
-
 def test_wrong_value_on_creation():
     with pytest.raises(Exception, match='1\\.3 is not a valid State'):
         ProcessingState(1.3)
 
+def test_has_lock_and_can_context():
+    state = ProcessingState()
+    with state.lock:
+        return True
+
+def test_cannot_redefine_lock():
+    state = ProcessingState()
+    with pytest.raises(ValueError):
+        state.lock = 'bad_string'
 
 def test_wrong_value_on_assignment():
     state = ProcessingState()
     with pytest.raises(Exception, match='Supplied value is not a State variable'):
         state.set(1.3)
 
+def test_wrong_value_has_value():
+    state = ProcessingState()
+    with pytest.raises(RuntimeError):
+        state.has_value('asdf')
+
+def test_wrong_value_has_value_list():
+    state = ProcessingState()
+    with pytest.raises(RuntimeError):
+        state.has_value((State.STEADY, 'jkl'))
 
 def test_wait_until():
     state = ProcessingState()
@@ -43,6 +63,21 @@ def test_wait_until():
     assert state.has_value(State.STEADY)
     worker.join()
 
+def test_wait_until_list():
+    state = ProcessingState()
+    worker = Worker(state)
+    worker.start()
+    state.wait_until((State.STEADY, State.IDLE))
+    assert state.has_value(State.STEADY)
+    worker.join()
+
+def test_wait_until_has_timeout():
+    state = ProcessingState()
+    worker = Worker(state)
+    worker.start()
+    state.wait_until(State.STEADY, 4)
+    assert state.has_value(State.STEADY)
+    worker.join()
 
 def test_wait_while():
     state = ProcessingState()
@@ -51,3 +86,37 @@ def test_wait_while():
     state.wait_while(State.BOOT)
     assert state.has_value(State.STEADY)
     worker.join()
+
+def test_wait_while_list():
+    state = ProcessingState()
+    worker = Worker(state)
+    worker.start()
+    state.wait_while((State.BOOT, State.IDLE))
+    assert state.has_value(State.STEADY)
+    worker.join()
+
+def test_wait_while_has_timeout():
+    state = ProcessingState()
+    worker = Worker(state)
+    worker.start()
+    state.wait_while(State.BOOT, 4)
+    assert state.has_value(State.STEADY)
+    worker.join()
+
+def test_probably_running():
+    state = ProcessingState()
+    for set_to in RUNNING_CONDITIONS:
+        state.set(set_to)
+        assert state.probably_running()
+
+def test_should_stop():
+    state = ProcessingState()
+    for set_to in STOPPING_CONDITIONS:
+        state.set(set_to)
+        assert state.should_stop()
+
+def test_inactive():
+    state = ProcessingState()
+    for set_to in INACTIVE_CONDITIONS:
+        state.set(set_to)
+        assert state.inactive()
