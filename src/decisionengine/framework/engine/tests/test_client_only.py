36bbf35251aa1cb3861a1347c5c64578a3f2a62d
Pat Riehecky <riehecky@fnal.gov>
2021-03-18 14:41:43 -0500

    Setuptools uses entry return value as an error msg
    Our unit tests use it to validate client behavior

--- src/decisionengine/framework/engine/tests/test_client_only.py
+++ src/decisionengine/framework/engine/tests/test_client_only.py
@@ -25,8 +25,18 @@ def test_client_with_no_server():
         "An error occurred while trying to access a DE server at 'http://localhost:8888'\n" + \
         "Please ensure that the host and port names correspond to a running DE instance."
 
+def test_client_with_no_server_verbose():
+    msg = de_client.main(['--status', '--verbose'])
+    if "Connection refused" not in msg and "Cannot assign requested address" not in msg:
+        raise ValueError(msg)
+
 def test_exclusive_options():
     assert de_client.main(['--force']) == \
         "The --force (-f) option may be used only with --kill-channel."
     assert de_client.main(['--timeout', '2']) == \
         "The --timeout option may be used only with --kill-channel."
+
+def test_client_err_returned_as_rc():
+    """no de server is running, so --status should error"""
+    msg = de_client.console_scripts_main(['--status'])
+    assert 'An error occurred' in msg
