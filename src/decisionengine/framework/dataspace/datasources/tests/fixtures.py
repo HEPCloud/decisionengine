70ab133ff92a82972bcefd36e989c1b373688b74
Pat Riehecky <riehecky@fnal.gov>
2021-05-05 11:41:23 -0500

    Fixup use of pytest_postgresql for version 3.0.0

--- src/decisionengine/framework/dataspace/datasources/tests/fixtures.py
+++ src/decisionengine/framework/dataspace/datasources/tests/fixtures.py
@@ -16,7 +16,7 @@ DE_SCHEMA = [os.path.dirname(os.path.abspath(__file__)) + "/../postgresql.sql", 
 # DE_DB_PORT assigned at random
 PG_PROG = factories.postgresql_proc(user=DE_DB_USER, password=DE_DB_PASS,
                                     host=DE_DB_HOST, port=None)
-DE_DB = factories.postgresql('PG_PROG', db_name=DE_DB_NAME, load=DE_SCHEMA)
+DE_DB = factories.postgresql('PG_PROG', dbname=DE_DB_NAME, load=DE_SCHEMA)
 
 
 @pytest.fixture
