6991f65f4ed6cea21198bd623180ffe9c9a086f9
Kyle Knoepfel <knoepfel@fnal.gov>
2021-06-15 16:30:34 -0500

    Restore product-name translation required for some source-proxy cases.

---
+++ src/decisionengine/framework/modules/tests/test_translate_product_name.py
@@ -0,0 +1,24 @@
+from decisionengine.framework.modules.translate_product_name import translate, translate_all
+
+import pytest
+
+def test_translate_none():
+    with pytest.raises(RuntimeError, match="does not match the supported pattern"):
+        translate("")
+    assert translate("old") == ("old", None)
+
+def test_translate_simple():
+    assert translate("old -> new") == ('old', 'new')
+
+def test_translate_with_underscores():
+    assert translate("old_v0 -> new_v1") == ('old_v0', 'new_v1')
+
+def test_translate_illegal_characters():
+    with pytest.raises(RuntimeError, match="does not match the supported pattern"):
+        translate("old-v0 -> new-v1")
+    with pytest.raises(RuntimeError, match="does not match the supported pattern"):
+        translate("old-<3=->=new-<3")
+
+def test_translate_all():
+    specs = ["old", "old1 -> new1"]
+    assert translate_all(specs) == {"old": "old", "old1": "new1"}
---
+++ src/decisionengine/framework/modules/translate_product_name.py
@@ -0,0 +1,39 @@
+# The functionality provided here supports the translation of one
+# string to another using the syntax 'old -> new', subject to the
+# following constraints:
+#
+#   - The 'old' and 'new' strings may contain the following
+#     characters: a-z, A-Z, 0-9, and _ (underscore).
+#
+#   - If '->' is provided, it must be surrounded by at least one space
+#     on either side.
+#
+# The behavior is the following:
+#
+#   - translate("old") == ("old", None)
+#   - translate("old -> new") == ("old", "new")
+#   - translate_all(["old", "old1 -> new1"]) == {"old": "old", "old1": "new1"}
+
+import re
+
+
+def translate(spec):
+    """Break apart the string 'old -> new' into a tuple ('old', 'new')"""
+    match = re.fullmatch(R'(\w+)(\s+\->\s+(\w+))?', spec)
+    if match is None:
+        raise RuntimeError(f"The specification '{spec}' does not match the supported pattern "
+                           '"old_name[ -> new_name]",\n'
+                           "where the product names can consist of the characters a-z, a-Z, 0-9, "
+                           "and an underscore '_'.\nIf an optional new name is specified, the '->' "
+                           "token must be surrounded by at least\none space on either side.")
+    return match.group(1, 3)
+
+
+def translate_all(specs):
+    result = {}
+    for entry in specs:
+        old, new = translate(entry)
+        if new is None:
+            new = old
+        result[old] = new
+    return result
