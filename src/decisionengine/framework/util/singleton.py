fb797de41bdb47d38547c9ac95e4f91a296966eb
Pat Riehecky <riehecky@fnal.gov>
2021-06-15 10:27:57 -0500

    Push Singleton into its own space

---
+++ src/decisionengine/framework/util/singleton.py
@@ -0,0 +1,66 @@
+import abc
+import gc
+import logging
+
+from threading import Lock
+from weakref import WeakValueDictionary
+
+LOCK = Lock()
+
+__all__ = [
+    "ScopedSingleton",
+    "Singleton",
+    "ScopedSingletonABC",
+    "SingletonABC",
+]
+
+
+class Singleton(type):
+    """
+    Singleton pattern using Metaclass with strong refs
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        klass = cls._instances.get(cls, None)  # get a strong ref if it exists
+
+        if klass is not None:
+            logging.getLogger().debug(f"found instance of {cls}")
+            return klass
+
+        with LOCK:  # so that threads can't make parallel versions
+            klass = cls._instances.get(cls, None)  # get a strong ref if it exists
+            if klass is not None:  # pragma: no cover
+                # this case can be very hard to hit if cls inits super fast
+                # with trivial unit tests, it isn't going to happen
+                logging.getLogger().debug(f"found instance of {cls} after lock")
+                return klass
+
+            logging.getLogger().debug(f"Making instance of {cls}")
+            klass = super(Singleton, cls).__call__(*args, **kwargs)  # make a strong ref
+            cls._instances[cls] = klass
+
+        return klass
+
+
+class SingletonABC(abc.ABCMeta, Singleton):
+    pass
+
+
+class ScopedSingleton(Singleton):
+    """
+    Singleton pattern using Metaclass with weak refs
+    """
+
+    _instances = WeakValueDictionary()
+
+    def __call__(cls, *args, **kwargs):
+        # pypy uses delayed garbage collection
+        # so we need to do an explicit garbage collection here
+        gc.collect()
+        return super(ScopedSingleton, cls).__call__(*args, **kwargs)
+
+
+class ScopedSingletonABC(abc.ABCMeta, ScopedSingleton):
+    pass
