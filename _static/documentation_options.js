033955296804db6d66b656939e5eebe32117a24c
GitHub Action <action@github.com>
2021-06-24 12:25:04 +0000

    Update doc to match 3b104ebabca7bd4fd1c349e8cd0513a3e6105fee

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.0rc0.post114+g4c5564d4',
+    VERSION: '1.7.0rc0.post115+g3b104eba',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
