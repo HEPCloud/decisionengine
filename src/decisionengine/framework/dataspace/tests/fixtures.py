ca48c91e0ca1eb3f7eabfeb11b7b3961b745733b
Pat Riehecky <riehecky@fnal.gov>
2021-06-28 14:32:32 -0500

    Ensure fixtures are cleaned up between invocations

--- src/decisionengine/framework/dataspace/tests/fixtures.py
+++ src/decisionengine/framework/dataspace/tests/fixtures.py
@@ -1,3 +1,4 @@
+import gc
 import pytest
 
 from decisionengine.framework.dataspace import dataspace as ds
@@ -65,4 +66,7 @@ def dataspace(request):
     my_ds = ds.DataSpace(config)
     load_sample_data_into_datasource(my_ds)
 
-    return my_ds
+    yield my_ds
+
+    del my_ds
+    gc.collect()
