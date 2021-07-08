8f48152d13db37bc6c800f02f36fce64d086e536
Pat Riehecky <riehecky@fnal.gov>
2021-07-08 08:25:08 -0500

    Rename taskmanager test nodb
    Start work on DB based tests

---
+++ src/decisionengine/framework/taskmanager/tests/fixtures.py
@@ -0,0 +1,21 @@
+from decisionengine.framework.dataspace.tests.fixtures import (
+    PG_DE_DB_WITH_SCHEMA,
+    PG_DE_DB_WITHOUT_SCHEMA,
+    PG_PROG,
+    DATABASES_TO_TEST,
+    SQLALCHEMY_PG_WITH_SCHEMA,
+    SQLALCHEMY_IN_MEMORY_SQLITE,
+    dataspace,
+)
+from decisionengine.framework.dataspace.datasources.tests.fixtures import mock_data_block
+
+__all__ = [
+    "PG_DE_DB_WITH_SCHEMA",
+    "PG_DE_DB_WITHOUT_SCHEMA",
+    "PG_PROG",
+    "DATABASES_TO_TEST",
+    "SQLALCHEMY_PG_WITH_SCHEMA",
+    "SQLALCHEMY_IN_MEMORY_SQLITE",
+    "dataspace",
+    "mock_data_block",
+]
