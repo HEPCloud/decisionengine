a2875a0bb6ab4023ef15b2a411351dda0311707d
GitHub Action <action@github.com>
2021-07-12 23:27:42 +0000

    Update doc to match 308343e9358075ea423b4494aa4b4e2ccf5eaef3

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.99.post2.post6+g23a4b770',
+    VERSION: '1.6.99.post2.post8+g308343e9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
