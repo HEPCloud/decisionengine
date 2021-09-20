2cacef4f4af23393e26e85630dd0af258031c92c
Shreyas Bhat <sbhat@fnal.gov>
2021-09-20 18:45:14 +0000

    Added check for proper metrics environment and associated unit tests

--- src/decisionengine/framework/engine/tests/test_startup.py
+++ src/decisionengine/framework/engine/tests/test_startup.py
@@ -1,6 +1,11 @@
+import pytest
 import tempfile
 
-from decisionengine.framework.engine.DecisionEngine import _get_global_config, parse_program_options
+from decisionengine.framework.engine.DecisionEngine import (
+    parse_program_options,
+    _get_global_config,
+    _check_metrics_env
+)
 
 # Because we just want to ensure correct behavior of program options
 # overriding a configuration, it is not necessary to have a valid DE
@@ -11,9 +16,24 @@ global_config_file = tempfile.NamedTemporaryFile()
 global_config_file.write(b"{}")
 global_config_file.flush()
 
+# For some of the metrics environment tests, we need to set up the environment
+
+
+@pytest.fixture()
+def metrics_env_setup(tmp_path, monkeypatch):
+    """Make sure we have a directory set for PROMETHEUS_MULTIPROC_DIR so that
+    metric instantiation gives us multiprocess metrics"""
+    # Get a fixed dir
+    d = tmp_path
+    monkeypatch.setenv("PROMETHEUS_MULTIPROC_DIR", str(d))
+    yield
+    monkeypatch.delenv("PROMETHEUS_MULTIPROC_DIR")
+
 # We do not call the DE's _get_de_conf_manager as that assumes the
 # presence of configuration directories, which are unnecessary for
 # this test.
+
+
 def _check_override(arguments):
     options = parse_program_options(arguments)
     return _get_global_config(global_config_file.name, options)
@@ -27,3 +47,33 @@ def test_default_config():
 def test_change_port():
     arguments = ["--port=54321"]
     assert _check_override(arguments) == {"server_address": ["localhost", 54321]}
+
+
+def test_check_metrics_env_no_webserver():
+    arguments = ['--no-webserver']
+    options = parse_program_options(arguments)
+    try:
+        _check_metrics_env(options)
+    except Exception as e:
+        pytest.fail(f"--no-webserver should have skipped the metrics env check,"
+                    " but instead we got an exception {e}")
+
+
+@pytest.mark.usefixtures("metrics_env_setup")
+def test_check_metrics_env_var_set():
+    arguments = []
+    options = parse_program_options(arguments)
+    try:
+        _check_metrics_env(options)
+    except Exception as e:
+        pytest.fail(f"PROMETHEUS_MULTIPROC_DIR should be set, so"
+                    " check_metrics_env should have passed but instead we got"
+                    " an exception {e}")
+
+
+def test_check_metrics_env_var_unset():
+    arguments = []
+    options = parse_program_options(arguments)
+    with pytest.raises(EnvironmentError) as excinfo:
+        _check_metrics_env(options)
+    assert "PROMETHEUS_MULTIPROC_DIR must be set" in str(excinfo.value)
