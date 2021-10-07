de1536fae56f2cd1bf03d4c65ece67d1ea2d4c18
Lisa Goodenough <goodenou@fnal.gov>
2021-10-07 23:37:15 +0000

    add dummy source and test
    (cherry picked from commit c1cb8258726042bb0217a0d89fde764baa0f965a)

---
+++ src/decisionengine/framework/modules/tests/test_EmptySource.py
@@ -0,0 +1,25 @@
+import pytest
+
+from decisionengine.framework.modules.EmptySource import EmptySource, pd
+
+
+def test_empty_source_structure():
+    params = {"data_product_name": "empty_data", "channel_name": "test"}
+    test_empty_source = EmptySource(params)
+
+    assert test_empty_source.get_parameters() == {"data_product_name": "empty_data", "channel_name": "test"}
+    assert test_empty_source.data_product_name == "empty_data"
+
+    assert test_empty_source._produces == {test_empty_source.data_product_name: pd.DataFrame}
+
+    key, df = test_empty_source.acquire().popitem()
+    assert key == test_empty_source.data_product_name
+    assert type(df) == pd.DataFrame
+    assert df.empty
+
+
+def test_missing_data_product_name_not_supported():
+    params = {"data_product_name": "", "channel_name": "test"}
+
+    with pytest.raises(RuntimeError, match="No data_product_name found in configuration"):
+        EmptySource(params)
