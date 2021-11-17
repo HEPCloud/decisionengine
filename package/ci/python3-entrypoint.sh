24ba52729afb9bac7bd303d51a20a7d3405a48c7
Pat Riehecky <riehecky@fnal.gov>
2021-11-17 15:04:51 -0600

    Add a redis server to the CI testing containers

--- package/ci/python3-entrypoint.sh
+++ package/ci/python3-entrypoint.sh
@@ -2,8 +2,22 @@
 CMD=${1:- -m pytest}
 LOGFILE=${2:- pytest.log}
 
+# this trap shouldn't eat the exit code of python
+function stop_redis {
+    /opt/rh/rh-redis5/root/usr/libexec/redis-shutdown
+}
+trap stop_redis EXIT
+
 id
 getent passwd $(whoami)
+echo ''
+
+# Start redis in the background, stop server via traps
+/opt/rh/rh-redis5/root/usr/bin/redis-server &
+sleep 1
+echo ''
+
+# Useful info
 python3 -m site
 echo ''
 
