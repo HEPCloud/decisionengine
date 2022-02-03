fff07e800a42c99536430df2489a2064b34f21b7
GitHub Action <action@github.com>
2022-02-03 16:20:27 +0000

    Update doc to match ca5ddf6f6da5b234edae0f5a8a4f6e205c0520e8

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.99.post2.dev6+ge60efe78',
+    VERSION: '1.7.99.post2.dev7+gca5ddf6f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
