ee2fe6133cc7c16dce9549ab943b119e108dd6de
Kyle Knoepfel <knoepfel@fnal.gov>
2022-10-03 16:49:45 +0000

    Add timing information for when publisher was disabled.

--- src/decisionengine/framework/taskmanager/PublisherStatus.py
+++ src/decisionengine/framework/taskmanager/PublisherStatus.py
@@ -1,25 +1,45 @@
 # SPDX-FileCopyrightText: 2017 Fermi Research Alliance, LLC
 # SPDX-License-Identifier: Apache-2.0
 
+from collections import namedtuple
+from datetime import datetime
+
+from tabulate import tabulate
+
+PublisherState = namedtuple("PublisherState", ["enabled", "duration", "since"])
+
 
 class PublisherStatus:
-    def __init__(self, enabled):
-        self._enabled = enabled
+    def __init__(self, status_snapshot):
+        self._status = status_snapshot
 
     def is_enabled(self, publisher_name):
-        return self._enabled[publisher_name]
+        return self._status[publisher_name][0]
+
+    def status(self, publisher_name):
+        is_enabled, time_of_update = self._status[publisher_name]
+        return PublisherState(enabled=is_enabled, duration=datetime.now() - time_of_update, since=time_of_update)
+
+    def __str__(self):
+        return tabulate([(k,) + v for k, v in self._status.items()], headers=["Publisher", "Enabled", "Since"])
 
 
 class PublisherStatusBoard:
     def __init__(self, publisher_names):
         # All publishers are enabled by default
-        self._enabled = dict.fromkeys(publisher_names, True)
+        self._status = dict.fromkeys(publisher_names, (True, datetime.now()))
 
     def update(self, publisher_name, result_of_publish):
         # It is a logical error to call update on a publisher whose
         # name has not been specified in the constructor.
-        assert publisher_name in self._enabled
-        self._enabled[publisher_name] = result_of_publish
+        assert publisher_name in self._status
+
+        # Do not update the status of the publisher if the cached
+        # status is the same as result_of_publish
+        if self._status.get(publisher_name)[0] == result_of_publish:
+            return
+
+        self._status[publisher_name] = (result_of_publish, datetime.now())
 
     def snapshot(self):
-        return PublisherStatus(self._enabled)
+        return PublisherStatus(self._status)
---
+++ src/decisionengine/framework/tests/test_publisher_status_board.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2017 Fermi Research Alliance, LLC
+# SPDX-License-Identifier: Apache-2.0
+
+import re
+import time
+
+from decisionengine.framework.taskmanager.PublisherStatus import PublisherStatusBoard
+
+
+def test_publisher_status_board():
+    board = PublisherStatusBoard(["a", "b"])
+    snapshot = board.snapshot()
+    a1_enabled, _, a1_since = snapshot.status("a")
+    b1_enabled, _, b1_since = snapshot.status("b")
+    assert a1_enabled and b1_enabled
+
+    board.update("a", False)
+
+    a2_enabled, _, a2_since = snapshot.status("a")
+    b2_enabled, _, b2_since = snapshot.status("b")
+    assert not a2_enabled and b2_enabled
+    assert a2_since > a1_since
+    assert b2_since == b1_since
+
+    re.search(r"a\s+False.*b\s+True", str(snapshot), re.DOTALL)
+
+    time.sleep(2)
+    assert board.snapshot().status("a").duration.seconds >= 2
