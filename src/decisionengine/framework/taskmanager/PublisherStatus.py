34cfef14e2343b695e5e3cfa681d92f852f7c3fa
Kyle Knoepfel <knoepfel@fnal.gov>
2022-10-17 17:08:47 +0000

    Add docstrings.

--- src/decisionengine/framework/taskmanager/PublisherStatus.py
+++ src/decisionengine/framework/taskmanager/PublisherStatus.py
@@ -1,22 +1,69 @@
 # SPDX-FileCopyrightText: 2017 Fermi Research Alliance, LLC
 # SPDX-License-Identifier: Apache-2.0
 
-from collections import namedtuple
-from datetime import datetime
+"""PublisherStatus
+
+The status of each decision-engine publisher is captured by a PublisherStatus object.
+The status can be queried to determine if a given publisher is enabled, and when it was last
+enabled or disabled.  To access this information from a datablock, one must specify the
+a consumes statement:
+
+..  code_block:: python
+    @Transform.consumes(publisher_status=PublisherStatus)
+    class MyTransform(Transform.Transform):
+        ...
+        def transform(self, datablock):
+            status = self.publisher_status(datablock)
+
+            if status.is_enabled("some_publisher"):
+                # some_publisher is enabled
+                ...
+            elif status.state("some_publisher").duration.seconds > 10:
+                # some_publisher has been disabled for at least 10 seconds
+                ...
+
+The API for each relevant class is given below.
+"""
+
+from datetime import datetime, timedelta
+from typing import NamedTuple
 
 from tabulate import tabulate
 
-PublisherState = namedtuple("PublisherState", ["enabled", "duration", "since"])
+
+class PublisherState(NamedTuple):
+    enabled: bool
+    duration: timedelta
+    since: datetime
+
+
+PublisherState.enabled.__doc__ = "Boolean value indicating if publisher is enabled."
+PublisherState.duration.__doc__ = (
+    "datetime.timedelta object representing duration between now and when publisher was last enabled/disabled"
+)
+PublisherState.since.__doc__ = "datetime.datetime object representing when publisher was last enabled/disabled"
 
 
 class PublisherStatus:
+    """Proxy object that provides publisher-status information."""
+
     def __init__(self, status_snapshot):
         self._status = status_snapshot
 
     def is_enabled(self, publisher_name):
+        """
+        :param str publisher_name: The name of the configured publisher
+        :return: If publisher is enabled or disabled
+        :rtype: bool
+        """
         return self._status[publisher_name][0]
 
-    def status(self, publisher_name):
+    def state(self, publisher_name):
+        """
+        :param str publisher_name: The name of the configured publisher
+        :return: Full state of publisher
+        :rtype: :class:`PublisherState`
+        """
         is_enabled, time_of_update = self._status[publisher_name]
         return PublisherState(enabled=is_enabled, duration=datetime.now() - time_of_update, since=time_of_update)
 
@@ -25,11 +72,21 @@ class PublisherStatus:
 
 
 class PublisherStatusBoard:
+    """Publisher status board owned by each decision channel
+
+    The status board is not a user-facing entity; it is owned by each decision
+    channel, which updates the status of each publisher after they have been run.
+    """
+
     def __init__(self, publisher_names):
         # All publishers are enabled by default
         self._status = dict.fromkeys(publisher_names, (True, datetime.now()))
 
     def update(self, publisher_name, result_of_publish):
+        """
+        :param str publisher_name: The name of the configured publisher
+        :param bool result_of_publish: Whether the last execution of the publisher was successsul
+        """
         # It is a logical error to call update on a publisher whose
         # name has not been specified in the constructor.
         assert publisher_name in self._status
@@ -42,4 +99,8 @@ class PublisherStatusBoard:
         self._status[publisher_name] = (result_of_publish, datetime.now())
 
     def snapshot(self):
+        """
+        :return: An publisher-status object corresponding to now
+        :rtype: :class:`PublisherStatus`
+        """
         return PublisherStatus(self._status)
