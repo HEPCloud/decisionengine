469f12df9188437aff9276a7052d83c78cda5fde
Shreyas Bhat <sbhat@fnal.gov>
2024-04-11 11:58:35 -0500

    Check if redis server is running.  If not, skip integration test.  Also marked applicable unit tests in this file

--- src/decisionengine/framework/engine/tests/test_verify_redis_server.py
+++ src/decisionengine/framework/engine/tests/test_verify_redis_server.py
@@ -2,10 +2,35 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
+import redis
 
 from decisionengine.framework.engine.DecisionEngine import _verify_redis_server, _verify_redis_url
 
 
+def _redis_server_running() -> bool:
+    r = redis.Redis("127.0.0.1", socket_timeout=0.1)
+    try:
+        r.ping()
+        return True
+    except Exception:
+        return False
+
+
+skip_if_no_redis = pytest.mark.skipif(
+    not _redis_server_running(), reason="No redis server running at default broker url"
+)
+
+
+@pytest.fixture
+def fake_redis_server(monkeypatch):
+    class mockRedis:
+        def ping(self):
+            return True
+
+    monkeypatch.setattr(redis.Redis, "from_url", lambda x: mockRedis())
+
+
+@pytest.mark.unit
 def test_verify_bad_url():
     bad_url = "Garbage"
     expected = (
@@ -16,6 +41,7 @@ def test_verify_bad_url():
         _verify_redis_url(bad_url)
 
 
+@pytest.mark.unit
 def test_verify_bad_broker():
     amqp_backend = "amqp://localhost:6379/0"
     expected = "Unsupported data-broker backend 'amqp'; only 'redis' is currently supported."
@@ -23,15 +49,23 @@ def test_verify_bad_broker():
         _verify_redis_url(amqp_backend)
 
 
+@pytest.mark.unit
 def test_verify_redis_url():
     _verify_redis_url("redis://localhost:6379/0")
     _verify_redis_url("redis://127.0.0.1:6379/0")
 
 
-def test_verify_redis_server():
+@skip_if_no_redis
+def test_verify_redis_server_int():
     _verify_redis_server("redis://localhost:6379/0")
 
 
+@pytest.mark.unit
+def test_verify_redis_server(fake_redis_server):
+    _verify_redis_server("redis://localhost:6379/0")
+
+
+@pytest.mark.unit
 def test_verify_bad_redis_server():
     url = "redis://localhost:1234/5"
     expected = f"A server with broker URL {url} is not responding."
