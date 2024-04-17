ff674229ebebaa42a2562427937ba56203797fbe
Shreyas Bhat <sbhat@fnal.gov>
2024-04-17 17:25:04 -0500

    Remove unit test marker

--- src/decisionengine/framework/engine/tests/test_verify_redis_server.py
+++ src/decisionengine/framework/engine/tests/test_verify_redis_server.py
@@ -30,7 +30,6 @@ def fake_redis_server(monkeypatch):
     monkeypatch.setattr(redis.Redis, "from_url", lambda x: mockRedis())
 
 
-@pytest.mark.unit
 def test_verify_bad_url():
     bad_url = "Garbage"
     expected = (
@@ -41,7 +40,6 @@ def test_verify_bad_url():
         _verify_redis_url(bad_url)
 
 
-@pytest.mark.unit
 def test_verify_bad_broker():
     amqp_backend = "amqp://localhost:6379/0"
     expected = "Unsupported data-broker backend 'amqp'; only 'redis' is currently supported."
@@ -49,7 +47,6 @@ def test_verify_bad_broker():
         _verify_redis_url(amqp_backend)
 
 
-@pytest.mark.unit
 def test_verify_redis_url():
     _verify_redis_url("redis://localhost:6379/0")
     _verify_redis_url("redis://127.0.0.1:6379/0")
@@ -60,12 +57,10 @@ def test_verify_redis_server_int():
     _verify_redis_server("redis://localhost:6379/0")
 
 
-@pytest.mark.unit
 def test_verify_redis_server(fake_redis_server):
     _verify_redis_server("redis://localhost:6379/0")
 
 
-@pytest.mark.unit
 def test_verify_bad_redis_server():
     url = "redis://localhost:1234/5"
     expected = f"A server with broker URL {url} is not responding."
