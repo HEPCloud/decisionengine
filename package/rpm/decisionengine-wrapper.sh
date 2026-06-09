029d913ef3a7976e75708cd218d5ed5afce487ff
Marco Mambelli <marcom@fnal.gov>
2026-06-09 12:15:32 -0500

    updated systemclt service with pre-service script

--- package/rpm/decisionengine-wrapper.sh
+++ package/rpm/decisionengine-wrapper.sh
@@ -12,6 +12,11 @@ DE_USER=decisionengine
 DE_CMD=$(basename "$0")
 DE_HOME=$(getent passwd "$DE_USER" | cut -d: -f6 )
 
+check_loop() {
+  # Make sure that the script is not re-invoking itself
+  [ "$0" = "$(command -v "$DE_CMD")" ]
+}
+
 # DECISIONENGINE_DEBUG - debug enabled if not empty (debug flags possible in the future)
 if echo "$*" | grep -- "--debug"; then
   DECISIONENGINE_DEBUG=true
@@ -27,12 +32,13 @@ if [ ! -f "$DE_EXPECTED" ]; then
     echo "The command will likely fail. Was the Python code installed? Use decisionengine-install-python"
 fi
 # Run the command, with su if needed. NOTE that decisionengine-init can run only as root
+# Including '-m' in the su command to preserve the environment. Systemd sets some variables for Prometheus and config
 if [ "$(id -u)" -eq 0 ]; then
     decisionengine-init.sh
-    # su -s /bin/bash -l $DE_USER -c "export PATH=\"\$HOME/.local/bin:\$PATH\"; echo \$PATH; command -v \"$DE_CMD\"; \"$DE_CMD\" $(for i in "$@"; do echo -n "\"$i\" "; done;); echo \"Test END\""
-    su -s /bin/bash -l -c "export PATH=\"\$HOME/.local/bin:\$PATH\"; \"$DE_CMD\" $(for i in "$@"; do printf "%s" "\"$i\" "; done;)" $DE_USER
+    # su -s /bin/bash -m -l $DE_USER -c "export PATH=\"\$HOME/.local/bin:\$PATH\"; echo \$PATH; command -v \"$DE_CMD\"; \"$DE_CMD\" $(for i in "$@"; do echo -n "\"$i\" "; done;); echo \"Test END\""
+    su -s /bin/bash -m -l -c "export PATH=\"\$DE_HOME/.local/bin:\$PATH\"; \"$DE_CMD\" $(for i in "$@"; do printf "%s" "\"$i\" "; done;)" $DE_USER
 elif [ "$(whoami)" = "$DE_USER" ]; then
-    export PATH="$HOME/.local/bin:$PATH"
+    export PATH="$DE_HOME/.local/bin:$PATH"
     "$DE_CMD" "$@"
 else
     echo "decisionengine commands can be invoked only as $DE_USER or root user"
